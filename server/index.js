7acc3b1857ad3eb3804be2a34114c38177016d25
Muhammed athimanil <hello@athimannil.com>
2018-02-01 12:49:00 +0530

    initial commit
    note: watch for index.html not working :-(

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import path from 'path';
+
+let app = express();
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.listen(3000, () => console.log('I am logged here with 3000'));
